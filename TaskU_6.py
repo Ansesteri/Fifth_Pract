c4bd97cb98fd11bd7d18944689607525006532b2
Ansesteri <angrylemon5678@gmail.com>
2022-11-05 16:41:01 +0200

    Update TaskU_6.py

--- TaskU_6.py
+++ TaskU_6.py
@@ -26,7 +26,40 @@ class Pet:
             print("Name has successfully changed.")
 
     def talk(self):
-        print("My name is", self.__name)
+        print("My name is", self.__name,
+        ", and I fell myself", self.mood)
+        self.__pass_time()
+    
+    def eat(self, food = 4):
+        print("Thanks!")
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        self.__pass_time()
+    
+    def play(self, fun = 4):
+        print("Yahoooo!!")
+        self.boredom -= fun
+        if self.boredom < 0:
+            self.boredom = 0
+        self.__pass_time()
+
+    def __pass_time(self):
+        self.hunger += 1
+        self.boredom += 1
+
+    @property
+    def mood(self):
+        unhappiness = self.hunger + self.boredom
+        if unhappiness < 5:
+            m = "amazing"
+        elif 5 <= unhappiness <= 10:
+            m = "good"
+        elif 11 <= unhappiness <= 15:
+            m = "bad"
+        else:
+            m = "awful"
+        return m
 
 def main():
     print('Access to attribute of class Pet.total:', end = " ")
@@ -40,6 +73,6 @@ def main():
     Pet.status()
 
     print("Access to attribute of class through odject:", end = " ")
-    print(pet1.total)    
+    print(pet1.mood)    
 
 main()
