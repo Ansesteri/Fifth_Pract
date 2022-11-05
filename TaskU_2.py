cf8331a13dccbce87f193ae5f0e6493db5dc27ce
Ansesteri <angrylemon5678@gmail.com>
2022-11-05 14:46:22 +0200

    TaskU_2.py

---
+++ TaskU_2.py
@@ -0,0 +1,15 @@
+class Pet:
+    """V-Pet"""
+    def __init__(self):
+        print('An animal was born')
+    
+    def talk(self):
+        print("Hi, I am an animal!")
+
+def main():
+    pet1 = Pet()
+    pet1.talk()
+    pet2 = Pet()
+    pet2.talk()
+
+main()
